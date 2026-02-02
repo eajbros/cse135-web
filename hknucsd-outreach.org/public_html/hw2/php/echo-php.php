2cf390c5868d72068d8668e8346f1d57c71d7733
niannianwang <niw002@ucsd.edu>
2026-02-01 22:48:48 -0800

    php echo

---
+++ hknucsd-outreach.org/public_html/hw2/php/echo-php.php
@@ -0,0 +1,36 @@
+<?php
+header("Cache-Control: no-cache");
+header("Content-Type: text/html");
+
+$protocol = $_SERVER['SERVER_PROTOCOL'] ?? '';
+$method   = $_SERVER['REQUEST_METHOD'] ?? '';
+$query    = $_SERVER['QUERY_STRING'] ?? '';
+$ua       = $_SERVER['HTTP_USER_AGENT'] ?? '';
+$ip       = $_SERVER['REMOTE_ADDR'] ?? '';
+$host     = $_SERVER['HTTP_HOST'] ?? '';
+$time     = date("r");
+
+/* Read raw body (works for POST / PUT / DELETE) */
+$body = file_get_contents("php://input");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>General Request Echo</title>
+</head>
+<body>
+  <h1 align="center">General Request Echo</h1>
+  <hr>
+
+  <p><b>Hostname:</b> <?= htmlspecialchars($host) ?></p>
+  <p><b>Time:</b> <?= htmlspecialchars($time) ?></p>
+  <p><b>User Agent:</b> <?= htmlspecialchars($ua) ?></p>
+  <p><b>IP Address:</b> <?= htmlspecialchars($ip) ?></p>
+
+  <p><b>HTTP Protocol:</b> <?= htmlspecialchars($protocol) ?></p>
+  <p><b>HTTP Method:</b> <?= htmlspecialchars($method) ?></p>
+  <p><b>Query String:</b> <?= htmlspecialchars($query) ?></p>
+  <p><b>Message Body:</b> <?= htmlspecialchars($body) ?></p>
+
+</body>
+</html>
